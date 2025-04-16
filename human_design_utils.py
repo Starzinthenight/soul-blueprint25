302efcee3687bc72d20fb41db2cdd2a7910da584
Starzinthenight <starzinthenight81@gmail.com>
2025-04-17 00:47:09 +0100

    Delete human_design_utils.py

--- human_design_utils.py
+++
@@ -1,9 +0,0 @@
-def get_human_design_type(date, time, place):
-    """
-    Placeholder function for calculating Human Design type.
-
-    In future versions, this will use real logic based on Human Design charts,
-    or API integration. For now, it returns a default type for testing.
-    """
-    # TODO: Replace with actual HD logic or API
-    return "Projector"
